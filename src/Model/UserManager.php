dc8a346d3833b39d6a90fc18e6d128ba03acd0dd
mathis <chartouni.mathis@gmail.com>
2021-11-03 11:11:15 +0100

    created user's profile page and linked it with the database

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 class userManager extends AbstractManager
 {
+
 public const TABLE = 'user';
 
 public function createUser(array $users):int
@@ -14,5 +15,14 @@ public function createUser(array $users):int
     $statement->bindValue(':email', $users['email'], \PDO::PARAM_STR);
     $statement->execute();
     return (int)$this->pdo->lastInsertId();
+}
+
+public function update(array $user): bool
+{
+    $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `username` = :username WHERE id=:id");
+    $statement->bindValue('id', $user['id'], \PDO::PARAM_INT);
+    $statement->bindValue('title', $user['username'], \PDO::PARAM_STR);
+
+    return $statement->execute();
 }
 }
