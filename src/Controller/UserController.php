40e9c5863b9a6c73967e42e4031c6173d9d03544
chbville <bonneville.charles@yahoo.fr>
2021-11-03 12:08:41 +0100

    Register Form back

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,30 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
+
 class UserController extends AbstractController
 {
+    public function register(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userManager = new UserManager();
+            $userManager->createUser($_POST);
+        }
+        return $this->twig->render('User/formRegister.html.twig');
+    }
+
+    public function update(array $user): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `username` = :username WHERE id=:id");
+        $statement->bindValue('id', $user['id'], \PDO::PARAM_INT);
+        $statement->bindValue('username', $user['username'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+
+    public function connect(): string
+    {
+        return $this->twig->render('User/formConnect/html/twig');
+    }
 }
--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -4,14 +4,15 @@ namespace App\Model;
 
 class userManager extends AbstractManager
 {
-public const TABLE = 'user';
+    public const TABLE = 'user';
 
-public function createUser(array $users):int
+    public function createUser(array $user):int
 {
-    $statement = $this->pdo->prepare("INSERT INTO " . static::TABLE . "(`username`,`email`,`password`,`create_time`)
-    VALUES(:username,:email,:password,NOW())");
-    $statement->bindValue(':username', $users['username'], \PDO::PARAM_STR);
-    $statement->bindValue(':email', $users['email'], \PDO::PARAM_STR);
+    $statement = $this->pdo->prepare("INSERT INTO " . static::TABLE . "(`username`,`email`,`password`,`created_at`, `is_admin`)
+    VALUES(:username,:email,:password,NOW(), 0)");
+    $statement->bindValue(':username', $user['username'], \PDO::PARAM_STR);
+    $statement->bindValue(':email', $user['email'], \PDO::PARAM_STR);
+    $statement->bindValue(':password', $user['password'], \PDO::PARAM_STR);
     $statement->execute();
     return (int)$this->pdo->lastInsertId();
 }
