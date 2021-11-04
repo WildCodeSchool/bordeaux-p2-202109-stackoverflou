58f951e08d8b2042adcb1c342328d44dc9a9f415
chbville <bonneville.charles@yahoo.fr>
2021-11-04 20:35:29 +0100

    form securisation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,19 +8,31 @@ class UserController extends AbstractController
 {
     public function register(): string
     {
+        $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $userManager = new UserManager();
-            $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $userId = $userManager->createUser($_POST);
-            $userData = $userManager->selectOneById($userId);
-            $_SESSION['user'] = $userData;
-            header('location: /user/create?id=' . $userId);
+            $user = array_map('trim', $_POST);
+            if (empty($user['username'])) {
+                $errors['usernameError'] = 'Le champs pseudo doit être remplie';
+            }
+            if (empty($user['email'])) {
+                $errors['emailError'] = 'Le champs mail doit être remplie';
+            }
+            if (empty($user['password'])) {
+                $errors['passwordError'] = 'Le champs mot de passe doit petre remplie';
+            }
+            if (empty($errors)) {
+                $userManager = new UserManager();
+                $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $userId = $userManager->createUser($_POST);
+                $userData = $userManager->selectOneById($userId);
+                $_SESSION['user'] = $userData;
+                header('location: /user/create?id=' . $userId);
+            }
         }
         return $this->twig->render('User/formRegister.html.twig', [
-            'register_success' => $_GET['register'] ?? null,
+            'register_success' => $_GET['register'] ?? null, 'errors' => $errors
         ]);
     }
-
 
     public function connect(): string
     {
@@ -33,9 +45,7 @@ class UserController extends AbstractController
                 var_dump('not ok');
             }
         }
-        return $this->twig->render('User/formConnect.html.twig', [
-            'session' => $_SESSION,
-        ]);
+        return $this->twig->render('User/formConnect.html.twig', ['session' => $_SESSION,]);
     }
 
     public function logout()
