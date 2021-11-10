eb9834efbd9bce30790a046db75318ddacc9a184
mathis <chartouni.mathis@gmail.com>
2021-11-10 11:11:58 +0100

    added and modified redirections.

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -15,14 +15,13 @@ class UserManager extends AbstractManager
         $statement->bindValue(':email', $user['email'], \PDO::PARAM_STR);
         $statement->bindValue(':password', $user['password'], \PDO::PARAM_STR);
         $statement->bindValue(':pseudo_github', $user['pseudo_github'], \PDO::PARAM_STR);
-        //var_dump($statement);die;
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
     }
 
     public function selectOneByEmail(string $email)
     {
-        $statement = $this->pdo->prepare('SELECT * FROM' . self::TABLE . 'WHERE email=:email');
+        $statement = $this->pdo->prepare('SELECT * FROM ' . self::TABLE . ' WHERE email=:email');
         $statement->bindValue(':email', $email, \PDO::PARAM_STR);
         $statement->execute();
         return $statement->fetch();
