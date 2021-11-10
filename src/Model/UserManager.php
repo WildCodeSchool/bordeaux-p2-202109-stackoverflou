6d29d93e363d17be21a245926f4682087878b1f2
chbville <bonneville.charles@yahoo.fr>
2021-11-10 10:46:21 +0100

    Answer modif

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -9,11 +9,12 @@ class UserManager extends AbstractManager
     public function createUser(array $user): int
     {
         $statement = $this->pdo->prepare("INSERT INTO " . static::TABLE .
-            "(`username`, `email`, `password`, `created_at`, `is_admin`)    
-        VALUES(:username, :email, :password, NOW(), 0)");
+            "(`username`, `email`, `password`, `created_at`, `is_admin`, `pseudo_github`)    
+        VALUES(:username, :email, :password, NOW(), 0, :pseudo_github)");
         $statement->bindValue(':username', $user['username'], \PDO::PARAM_STR);
         $statement->bindValue(':email', $user['email'], \PDO::PARAM_STR);
         $statement->bindValue(':password', $user['password'], \PDO::PARAM_STR);
+        $statement->bindValue(':pseudo_github', $user['pseudo_github'], \PDO::PARAM_STR);
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
     }
